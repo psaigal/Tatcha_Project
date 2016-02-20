f3b28aa9651c169a2c44194efc7b51d621e55d93
Priyanka Saigal <email": null>
2016-02-19 22:16:21 -0800

    Test .env hide

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -10,7 +12,7 @@ var users = require('./routes/users');
 var search = require('./routes/search')
 
 var app = express();
-require('dotenv').config();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
