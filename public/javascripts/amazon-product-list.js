28c554aebb44fb1557408e5982cc33b24e965e40
Priyanka Saigal <email": null>
2016-02-21 11:24:13 -0800

    Add bootstrap and search features

--- public/javascripts/amazon-product-list.js
+++ public/javascripts/amazon-product-list.js
@@ -5,15 +5,27 @@ $( document ).ready(function() {
     $('#submit').click(function(event) {
       var userInput = $('#product_name').val();
       var formData = {searchTerm: userInput};
+
       event.preventDefault();
       var request = $.ajax({
         url: '/search',
         type: 'POST',
         data: formData
-      });;
+      });
       request.done(function(response){
-        console.log(response);
-        // $(".container").append(response);
+        console.log(response)
+        $("#container").html("");
+        for (var i = 0; i < response.length; i++ ){
+          var img= document.createElement("img");
+          img.src= response[i].MediumImage[0].URL[0]
+
+          $("#container").append(response[i].DetailPageURL[0] + "<br>");
+          $("#container").append(response[i].ItemAttributes[0].Title[0] + "<br>");
+          $("#container").append(img);
+          $("#container").append(response[i].ItemAttributes[0].Brand[0] + "<br>" + "<br>");
+        };
       });
   });
 });
+
+
--- routes/search.js
+++ routes/search.js
@@ -1,26 +1,50 @@
 var express = require('express');
 var router = express.Router();
-// var amazon = require('amazon-product-api');
-var AmazonAPI = require('amz-products');
+// var AmazonAPI = require('amz-products');
+var amazon = require('amazon-product-api');
 
 
 router.post('/', function (req, res) {
   var data = req.body //data: formData
   var searchTerm = data.searchTerm //userInput
-  res.send("HELLO WORLD");
-  var amazon = new AmazonAPI({
+
+
+  var client = amazon.createClient({
     awsId: process.env.AccessID,
     awsSecret: process.env.SecretKey,
     awsTag: process.env.AssociateTag
   });
 
+  var x = client.itemSearch({
+  searchIndex: 'All',
+  keywords: searchTerm,
+  responseGroup: 'ItemAttributes,Offers,Images'
+  }, function(err, results, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(results);  // products
+      // console.log(response); // response (containing TotalPages, TotalResults, MoreSearchResultsUrl and so on)
+    }
 
- amazon.getItemsInBrowseNode({
-    'SearchIndex': 'All',
-    'Keywords': searchTerm
-  }, function(err, res){
-      console.log(res)
+    // res.send(x)
   });
+
+
+
+   // //somehow you need to make a formatted query string, => use to make a GET request to the amazon api
+   //  var x = amazon.getItemsInBrowseNode({
+   //    Service:"AWSECommerceService",
+   //    Operation:"ItemSearch",
+   //    SearchIndex:"All",
+   //    Keywords: searchTerm
+   //  },
+   //  function(err, res){
+   //    console.log(res)
+   //    res.send(x);//send amazon api response back to client
+   //  });//end amazon.getitems
+
+  // res.send(x)
 });
 
 
