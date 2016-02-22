e6506704ee493b7afe3bae3c3a8a8229030973ad
Priyanka Saigal <email": null>
2016-02-21 18:57:40 -0800

    Style product list

--- public/javascripts/amazon-product-list.js
+++ public/javascripts/amazon-product-list.js
@@ -26,19 +26,55 @@ $( document ).ready(function() {
       });
       request.done(function(response){
         console.log(response)
-        $("#container").html("");
-        for (var i = 0; i < response.length; i++ ){
-          var img= document.createElement("img");
-          img.src= response[i].MediumImage[0].URL[0]
-          $(.col-md-4.col-sm-6.col-xs-12)
-          $("#container").append(response[i].DetailPageURL[0] + "<br>");
-          $("#container").append(response[i].ItemAttributes[0].Title[0] + "<br>");
-          $("#container").append(img);
-          $("#container").append(response[i].ItemAttributes[0].Brand[0] + "<br>" + "<br>");
+        $(".container-fluid").html("");
+        var counter = 1
+        for (var i = 0; i < response.length; i++){
+          var product = document.createElement("div");
+          product.className = "col-md-4";
+          var img = document.createElement("img");
+          var a = document.createElement('a');
+          var linkText = document.createTextNode("Shop on Amazon");
+          a.appendChild(linkText);
+          a.title = "Shop on Amazon";
+          a.href = response[i].DetailPageURL[0];
+          img.src = response[i].MediumImage[0].URL[0];
+          $(product).append(img);
+          $(product).append(response[i].ItemAttributes[0].Title[0] + "<br>");
+          // $(product).append(response[i].DetailPageURL[0] + "<br>");
+          $(product).append(a);
+          // $(product).append(response[i].ItemAttributes[0].Brand[0] + "<br>" + "<br>");
+          // if(product.id === "col" + (counter)|| product.id === "col"+ counter - 1){
+          $('.container-fluid').append(product);
+          // }
+          // if(i % 2 === 0){
+          // // var counter = 1;
+          //     var row = document.createElement("div")
+          //     row.className = "row";
+          //     row.id = "row" + counter;
+          //     $('#results').append(row);
+          //     counter ++
+          //   }
         };
+        while(($children = $(':not(.row)>.col-md-4:lt(2)')).length) {
+        $children
+            .parent()
+            .append(
+                $('<div class="row"></div>')
+                    .append($children.remove())
+        );
+        }
       });
     });
   });
 
 
+
+
+
+
+
+    // row = document.createElement("div");
+          // row.className = "row";
+          // $("#results").append(row);
+          // for (var j = 0; j < response.length; j++)
 
