0ba6bf6840a1095b849cc65516b25db7a64304ae
Priyanka Saigal <email": null>
2016-02-22 12:04:29 -0800

    Add amazon show now image

--- public/javascripts/amazon-product-list.js
+++ public/javascripts/amazon-product-list.js
@@ -1,15 +1,4 @@
 $( document ).ready(function() {
-  $('#downClick').on('click', function(event) {
-    var target = $('#downClick');
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top + 100
-        }, 500);
-    }
-  });
-
-
     $('#submit').click(function(event) {
       document.getElementsByClassName("home")[0].style.display = "none";
       var userInput = $('#product_name').val();
@@ -21,27 +10,31 @@ $( document ).ready(function() {
         type: 'POST',
         data: formData
       });
+
       request.done(function(response){
-        console.log(response)
+        // console.log(JSON.stringify(response))
         $(".container-fluid").html("");
         var counter = 1
         for (var i = 0; i < response.length; i++){
           var product = document.createElement("div");
           product.className = "col-md-4";
-          var img = document.createElement("img");
-          img.src = response[i].MediumImage[0].URL[0];
-          img.className = "product-image"
+          var product_img = document.createElement("img");
+          product_img.src = response[i].MediumImage[0].URL[0];
+          product_img.className = "product-image";
+          var amazon_img = new Image();
+          amazon_img.src = '/Amazon-Button.png';
           var a = document.createElement('a');
-          a.src = response[i].DetailPageURL[0];
-          a.className = "product-link"
-          var linkText = document.createTextNode("Shop on Amazon");
-          a.appendChild(linkText);
-          a.title = "Shop on Amazon";
+          a.href = response[i].DetailPageURL[0];
+          console.log(a)
+          amazon_img.className = "product-link"
+          amazon_img.onclick = function() {
+              window.location.href = a;
+          };
           $(product).append("<p id='product-description'>" + response[i].ItemAttributes[0].Title[0] + "</p>");
           $(product).append("<div class='line_break'></div>");
-          $(product).append(img);
+          $(product).append(product_img);
           $(product).append("<br>")
-          $(product).append(a);
+          $(product).append(amazon_img);
           $('.container-fluid').append(product);
         };
         while(($children = $(':not(.row)>.col-md-4:lt(2)')).length) {
@@ -54,6 +47,16 @@ $( document ).ready(function() {
         }
       });
     });
+
+  $('#downClick').on('click', function(event) {
+    var target = $('#downClick');
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top + 100
+        }, 500);
+    }
+  });
   });
 
 
